fd05bba94677e11116dde186365087d1706c81d6
Mahnoor <mahnoor.javed@tintash.com>
2021-09-25 06:39:24 +0500

    Refactor success code

--- paper-dashboard-react-main/src/components/success.js
+++ paper-dashboard-react-main/src/components/success.js
@@ -1,27 +1,41 @@
-import '../success.css';
+import React from 'react';
+import { Card, CardTitle, Row, Col } from 'reactstrap';
 
 const ThankYouPage = () => {
   return (
-    <div className="success-container">
-      <div className="flex tick-icon">
-        <i class="fa fa-check fa-5x"></i>
-      </div>
-      <h1 className="heading">Thank you, enjoy !</h1>
-      <div className="flex">
-        <h3 className="description">Your Order Has been Placed</h3>
-      </div>
-      <div className="flex">
-        <h4 className="shopping-btn">
-          <i className="fa fa-long-arrow-left shopping-btn-icon" aria-hidden="true"></i>
-          Continue Shopping
-        </h4>
-      </div>
-      <div className="flex">
-        <h3 className="description">
-          If you have any issues <span>Contact us</span>
-        </h3>
-      </div>
-    </div>
+    <>
+      <Row>
+        <Col md="8">
+          <Card className="card-user">
+            <div className="success-container">
+              <div className="flex tick-icon">
+                <i className="fa fa-check fa-5x" />
+              </div>
+              <CardTitle className="heading" tag="h1">
+                Thank you, enjoy !
+              </CardTitle>
+              <div className="flex">
+                <h3 className="description">Your Order Has been Placed</h3>
+              </div>
+              <div className="flex">
+                <div className="shopping-btn">
+                  <i
+                    className="fa fa-long-arrow-left shopping-btn-icon"
+                    aria-hidden="true"
+                  />
+                  Continue Shopping
+                </div>
+              </div>
+              <div className="flex">
+                <h3 className="description">
+                  If you have any issues <span>Contact us</span>
+                </h3>
+              </div>
+            </div>
+          </Card>
+        </Col>
+      </Row>
+    </>
   );
 };
 
--- paper-dashboard-react-main/src/routes.js
+++ paper-dashboard-react-main/src/routes.js
@@ -1,5 +1,42 @@
+import { Card, CardTitle, Row, Col } from 'reactstrap';
 
-var routes = [
-  
-];
-export default routes;
+const ThankYouPage = () => {
+  return (
+    <>
+      <Row>
+        <Col md="8">
+          <Card className="card-user">
+            <div className="success-container">
+              <div className="flex tick-icon">
+                <i className="fa fa-check fa-5x" />
+              </div>
+              <CardTitle className="heading" tag="h1">
+                Thank you, enjoy !
+              </CardTitle>
+
+              <div className="flex">
+                <h3 className="description">Your Order Has been Placed</h3>
+              </div>
+              <div className="flex">
+                <div className="shopping-btn">
+                  <i
+                    className="fa fa-long-arrow-left shopping-btn-icon"
+                    aria-hidden="true"
+                  />
+                  Continue Shopping
+                </div>
+              </div>
+              <div className="flex">
+                <h3 className="description">
+                  If you have any issues <span>Contact us</span>
+                </h3>
+              </div>
+            </div>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ThankYouPage;
