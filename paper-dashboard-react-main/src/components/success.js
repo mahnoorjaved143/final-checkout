36ab439b19f3bf74d6cf67ea67f229c27f592517
Mahnoor <mahnoor.javed@tintash.com>
2021-09-25 06:28:12 +0500

    Add success page

--- paper-dashboard-react-main/src/components/success.js
+++ paper-dashboard-react-main/src/components/success.js
@@ -2,22 +2,22 @@ import '../success.css';
 
 const ThankYouPage = () => {
   return (
-    <div className="sucess-container ">
-      <div className="flex tick">
+    <div className="success-container">
+      <div className="flex tick-icon">
         <i class="fa fa-check fa-5x"></i>
       </div>
       <h1 className="heading">Thank you, enjoy !</h1>
       <div className="flex">
-        <h3 className="lower-heading">Your Order Has been Placed</h3>
+        <h3 className="description">Your Order Has been Placed</h3>
       </div>
       <div className="flex">
-        <h4 className="heading2">
-          <i className="fa fa-long-arrow-left" aria-hidden="true"></i>
+        <h4 className="shopping-btn">
+          <i className="fa fa-long-arrow-left shopping-btn-icon" aria-hidden="true"></i>
           Continue Shopping
         </h4>
       </div>
       <div className="flex">
-        <h3 className="lower-heading">
+        <h3 className="description">
           If you have any issues <span>Contact us</span>
         </h3>
       </div>
